8ec4b5a3bfee20e429b10ceefc18d3d34a14ffa6
Nylah Safforld <nylahsafforld@Nylahs-MacBook-Air.local>
2019-12-09 14:43:16 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTask_name() {
+        return task_name;
+    }
+
+    public void setTask_name(String task_name) {
+        this.task_name = task_name;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @NotNull
+    @Size(min=4)
+    private String task_name;
+
+    @NotNull
+    @Size(min=3)
+    private String priority;
+
+    @NotNull
+    @Size(min=2)
+    private String date;
+
+
+
+
+}
